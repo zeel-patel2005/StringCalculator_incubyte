b7a7218e31aa98c1780a1b69099f70567d837850
zeel patel <zeelp4797@gmail.com>
2025-07-04 18:50:18 +0530

    Initial commit

---
+++ src/main/java/org/example/SimpleCalculator.java
@@ -0,0 +1,10 @@
+package org.example;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class SimpleCalculator {
+    public static void main(String[] args) {
+        System.out.println("TDD");
+    }
+}
