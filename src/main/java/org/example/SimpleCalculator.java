3ecab716b1fb16d859afc5b70f0fd4d5b563bc2f
zeel patel <zeelp4797@gmail.com>
2025-07-04 21:38:23 +0530

    Impl: Handle custom delimiters

--- src/main/java/org/example/SimpleCalculator.java
+++ src/main/java/org/example/SimpleCalculator.java
@@ -2,15 +2,22 @@ package org.example;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.regex.Pattern;
 
 public class SimpleCalculator {
     public static int add(String numbers) {
-        if (numbers.isEmpty()) return 0;
-        String[] nums = numbers.split("[,\n]");
+        String delimiter = "[,\n]";
+        if (numbers.startsWith("//")) {
+            int index = numbers.indexOf("\n");
+            delimiter = Pattern.quote(numbers.substring(2, index));
+            numbers = numbers.substring(index + 1);
+        }
+        String[] nums = numbers.split(delimiter);
         int sum = 0;
         for (String num : nums) {
             sum += Integer.parseInt(num);
         }
         return sum;
     }
+
 }
